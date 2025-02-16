dcd1bea7aa3d1c6bf711a0a3aa83f1b9aafac606
EmadDeve20 <emaddeve20@gmail.com>
2025-02-16 22:00:19 +0330

    create a function to create user

--- hoopoe/users/services.py
+++ hoopoe/users/services.py
@@ -1,18 +1,28 @@
 from django.db import transaction 
-from .models import BaseUser, Profile
+from .models import User
 
 
-def create_profile(*, user:BaseUser, bio:str | None) -> Profile:
-    return Profile.objects.create(user=user, bio=bio)
+# def create_profile(*, user:BaseUser, bio:str | None) -> Profile:
+#     return Profile.objects.create(user=user, bio=bio)
 
-def create_user(*, email:str, password:str) -> BaseUser:
-    return BaseUser.objects.create_user(email=email, password=password)
+# def create_user(*, email:str, password:str) -> BaseUser:
+#     
 
 
 @transaction.atomic
-def register(*, bio:str|None, email:str, password:str) -> BaseUser:
+def register_user(*, email:str, password:str) -> User:
+    """
+    register user is a function to create User object.
 
-    user = create_user(email=email, password=password)
-    create_profile(user=user, bio=bio)
+    Args:
+        email (str): user email.
+        password (str): user password.
+
+    Returns:
+        User: return Created User object
+    """
+
+    user = User.objects.create(email=email)
+    user.set_password(password)
 
     return user
