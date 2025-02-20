35d6d5734c563f9fc77c29f872765c763d2c7f82
EmadDeve20 <emaddeve20@gmail.com>
2025-02-21 00:39:21 +0330

    add url for authentication app

--- hoopoe/api/urls.py
+++ hoopoe/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include(('hoopoe.users.urls', 'users')))
+    path('', include(('hoopoe.users.urls', 'users'))),
+    path('', include(('hoopoe.authentication.urls', 'authentication'))),
 ]
