5a1e876a4edf68ddaffaa7ed2454f7d41ade6085
EmadDeve20 <emaddeve20@gmail.com>
2025-02-21 12:23:55 +0330

    create an url to change my password api

--- hoopoe/users/urls.py
+++ hoopoe/users/urls.py
@@ -6,6 +6,8 @@ app_name = "users"
 urlpatterns = [
     path('register/', apis.RegisterApi.as_view(),name="register"),
     path('my-profile/', apis.MyProfileApi.as_view(),name="my_profile"),
+    path('my-profile/change_password/', 
+         apis.ChangeMyPassword.as_view(),name="my_profile_change_password"),
     path('users-profile/<str:username>/', apis.UsersProfile.as_view(),name="users_profile"),
 
 ]
