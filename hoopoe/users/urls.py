7406cb3c06f67ca356ad4e9230f370c153febbe4
EmadDeve20 <emaddeve20@gmail.com>
2025-02-21 11:59:15 +0330

    create an url to get profile of a user

--- hoopoe/users/urls.py
+++ hoopoe/users/urls.py
@@ -5,5 +5,7 @@ app_name = "users"
 
 urlpatterns = [
     path('register/', apis.RegisterApi.as_view(),name="register"),
-    path('profile/', apis.MyProfileApi.as_view(),name="profile"),
+    path('my-profile/', apis.MyProfileApi.as_view(),name="my_profile"),
+    path('users-profile/<str:username>/', apis.UsersProfile.as_view(),name="users_profile"),
+
 ]
