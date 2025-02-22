90ed90bb4ac209503f8bbd3b63a70fc5ce305e21
EmadDeve20 <emaddeve20@gmail.com>
2025-02-22 23:12:41 +0330

    create initializer command

---
+++ hoopoe/commands/management/commands/init.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+
+from hoopoe.users.commands import UserCommands
+
+class Command(BaseCommand):
+    
+    help = "This is An Initializer. run it in first time in your first run."
+
+    def handle(self, *args, **options):
+        
+        user_commands = UserCommands()
+
+# ============================== User Commands ============================ 
+
+        if user_commands.create_default_profile_image():
+            self.stdout.write(
+                self.style.SUCCESS("added Default Profile Image Successfuly.")
+            )
+        else:
+            self.stdout.write(
+                self.style.SUCCESS("failed to add Default Profile Image!")
+            )
