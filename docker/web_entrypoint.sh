8acce1c557f0d9a44b2cbccdc68b691b7e851752
EmadDeve20 <emaddeve20@gmail.com>
2025-02-23 23:15:05 +0330

    update docker files

--- docker/web_entrypoint.sh
+++ docker/web_entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-echo "--> Waiting for db to be ready"
-./wait-for-it.sh db:5432
+# echo "--> Waiting for db to be ready"
+# ./wait-for-it.sh db:5432
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py makemigration
 python manage.py migrate
 python manage.py collectstatic --clear --noinput
 python manage.py collectstatic --noinput
+python manage.py init
+
 
 # Start server
 echo "--> Starting web process"
