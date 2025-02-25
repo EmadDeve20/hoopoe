710ead2850683cfad0836d1fea4508dfd65d5b81
EmadDeve20 <emaddeve20@gmail.com>
2025-02-26 01:34:32 +0330

    create url for chat socket

--- hoopoe/websocket/routings.py
+++ hoopoe/websocket/routings.py
@@ -1,3 +1,14 @@
+from django.urls import path
+
+from hoopoe.websocket.middleware.jwt_auth import AdminJWTAuthMiddlewareStack
+from hoopoe.websocket.consumer_chat import ChatConsumer
+
+app_name = "websocket"
+
 websocket_urlpatterns = [
-    
+    path(
+        "ws/chat-socket/",
+        AdminJWTAuthMiddlewareStack(ChatConsumer.as_asgi()),
+        name="chat_socket"
+    )
 ]
