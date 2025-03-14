970629c495b709b67447758799f5402ab35e7a24
EmadDeve20 <emaddeve20@gmail.com>
2025-03-14 12:31:48 +0330

    add Default to run mongodb

--- config/settings/mongodb.py
+++ config/settings/mongodb.py
@@ -4,9 +4,9 @@ from config.env import env
 
 MONGO_HOST = env("MONGO_HOST", default="127.0.0.1")
 MONGO_PORT = env.int("MONGO_PORT", default=27017)
-MONGO_DATABASE = env("MONGO_DATABASE")
-MONGO_USERNAME = env("MONGO_USERNAME")
-MONGO_PASSWORD = env("MONGO_PASSWORD")
+MONGO_DATABASE = env("MONGO_DATABASE", default="hoopoe_mongo")
+MONGO_USERNAME = env("MONGO_USERNAME", default="hoopoe_mongo")
+MONGO_PASSWORD = env("MONGO_PASSWORD", default="hoopoe_mongo")
 
 
 mongo_client = pymongo.MongoClient(
