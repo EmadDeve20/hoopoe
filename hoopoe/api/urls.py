284d014d2e720ded2187964210699a378d8309b0
EmadDeve20 <emaddeve20@gmail.com>
2025-03-14 11:56:48 +0330

    add url for chat_messages app

--- hoopoe/api/urls.py
+++ hoopoe/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path("", include(("hoopoe.users.urls", "users"))),
     path("", include(("hoopoe.authentication.urls", "authentication"))),
+    path("", include(("hoopoe.chat_messages.urls", "chat_messages"))),
 ]
